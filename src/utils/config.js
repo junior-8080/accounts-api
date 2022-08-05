b9f2d128fff7ff685b0f0110813b1a4edceb851e
Abdul Mukhsin <abdul@esoko.com>
2022-08-05 20:38:38 +0000

    updated env names

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config({path:'/home/abdul/backend/apis/node/authentication/.env'})
 
 module.exports = {
-    appPort : process.env.APP_PORT,
+    appPort : process.env.PORT,
     mongodbName: process.env.MONGO_DB_NAME,
     mongodbURL: process.env.MONGO_DB_URL,
     webBaseUrl : process.env.AUTH_WEB_BASE_URL,
